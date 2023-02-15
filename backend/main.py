0b43a45fa69982b22681f88556ed37548d75fee4
Salah-Eltenehy <91028700+Salah-Eltenehy@users.noreply.github.com>
2023-02-16 00:35:02 +0200

    update model.... there is an error in plots

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,13 @@
+
+
 from model.Model import Model
 
 m = Model()
 m.run_model()
-
+# #
 from flask import Flask
-from flask import request
-
+# from flask import request
+#
 app = Flask(__name__)
 
 
@@ -13,7 +15,7 @@ app = Flask(__name__)
 # def test(gene1, gene2, gene3, gene4, mutation):
 #     res = m.test_gene_from_front(gene1, gene2, gene3, gene4, mutation)
 #     return {"response": res}
-
+#
 @app.route("/test/<g1>/<g2>/<g3>/<g4>/<mu>", methods=['POST', 'GET'])
 def home(g1, g2, g3, g4, mu):
     # g1 = request.args.get('gene1')
@@ -24,6 +26,6 @@ def home(g1, g2, g3, g4, mu):
     res = m.test_gene_from_front(float(g1), float(g2), float(g3), float(g4), float(mu))
     # print(type(float(g1)), "  ", g2, " ", g3, " ", g4, " ", mu)
     return {"response": res}
-
-
+#
+#
 app.run(debug=False)
