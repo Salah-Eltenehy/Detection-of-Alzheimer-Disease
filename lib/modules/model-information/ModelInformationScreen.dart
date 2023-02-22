f60a1b5fe78d8879f299d6eda370882515a7c210
Salah-Eltenehy <91028700+Salah-Eltenehy@users.noreply.github.com>
2023-02-22 02:26:31 +0200

    finish frontend

--- lib/modules/model-information/ModelInformationScreen.dart
+++ lib/modules/model-information/ModelInformationScreen.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../shared/constants/Constants.dart';
 
 class ModelInformationScreen extends StatelessWidget {
-
+  _launchURL() async {
+    const url = 'https://en.wikipedia.org/wiki/K-means_clustering';
+    final uri = Uri.parse(url);
+    if (await canLaunchUrl(uri)) {
+      await launchUrl(uri);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,34 +30,53 @@ class ModelInformationScreen extends StatelessWidget {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: ListView(
-              children: const [
-                Text(
+              children: [
+                const Text(
                     'About algorithm used',
                   style: TextStyle(
                     fontWeight: FontWeight.bold,
                     fontSize: 26
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 8,
                 ),
-                Text(
-                    'Information about Kmeans clustring \n//TODO'
+                Column(
+                  children: [
+                    const Text(
+                        '''
+Information about Kmeans clustring:
+  k-means clustering is a method of vector quantization, originally from signal processing, that aims to partition n observations into k clusters in which each observation belongs to the cluster with the nearest mean (cluster centers or cluster centroid), serving as a prototype of the cluster. This results in a partitioning of the data space into Voronoi cells.
+'''
+                        ,style: TextStyle(
+                                fontSize: 20
+                                )   ,
+                    ),
+                    Row(
+                      children: [
+                        Spacer(),
+                        TextButton(
+                            onPressed: _launchURL,
+                            child: const Text('For more Information ?')
+                        ),
+                      ],
+                    ),
+                  ],
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 16,
                 ),
-                Text(
+                const Text(
                   'About Number of Clusters',
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 26
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 16,
                 ),
-                SizedBox(
+                const SizedBox(
                   width: 300,
                   height: 400,
                   child:  Image(
@@ -56,43 +84,50 @@ class ModelInformationScreen extends StatelessWidget {
                       image: AssetImage('assets/images/clusters.jpg'),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 8,
                 ),
-                Text(
+                const Text(
 '''
 As we mention above. KMeans algorithm depends on number of clusters. But actually we don\'t know the
 optimal number of clusters for our dataset. So we user Elbow method which is a graphical method to know the optimal number
 of clusters. As in the above image: the optimal number is 3. Why 3?\nScientists do experiments about that and conclude that ...//TODO   
 '''
+                  ,style: TextStyle(
+                    fontSize: 20
                 ),
-                SizedBox(
+                ),
+                const SizedBox(
                   height: 16,
                 ),
-                Text(
+                const Text(
                   'About Clusters',
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 26
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   width: 300,
                   height: 400,
                   child:  Image(
                     fit: BoxFit.fill,
-                    image: AssetImage('assets/images/clusters.jpg'),
+                    image: AssetImage('assets/images/samples.jpg'),
                   ),
                 ),
-                SizedBox(
+                const SizedBox(
                   height: 8,
                 ),
-                Text(
+                const Text(
                     '''
-As we mention above. KMeans algorithm depends on number of clusters. But actually we don\'t know the
-optimal number of clusters for our dataset. So we user Elbow method which is a graphical method to know the optimal number
-of clusters. As in the above image: the optimal number is 3. Why 3?\nScientists do experiments about that and conclude that ...//TODO   
+After training the model:
+  1- Te accuracy about 89%
+  2- The model classify the genes into three clusters as it the optimal number for these data set.
+     
 '''
+                    ,style: TextStyle(
+                  fontSize: 20
+                            ),
                 ),
               ],
             ),
--- lib/shared/constants/Constants.dart
+++ lib/shared/constants/Constants.dart
@@ -3,3 +3,5 @@ import 'package:flutter/material.dart';
 Color navBarColor = Colors.cyan;
 
 double PAGEWIDTH = 800;
+
+double TEXTSIZE = 20;
